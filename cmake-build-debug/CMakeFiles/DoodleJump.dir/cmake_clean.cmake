1618263f13496a1e1a4deb3c02b5d3e96b6a4453
Kianeki <48513668+Kianeki@users.noreply.github.com>
2021-12-06 12:07:06 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DoodleJump.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DoodleJump.dir/main.cpp.obj"
+  "DoodleJump.exe"
+  "DoodleJump.exe.manifest"
+  "DoodleJump.pdb"
+  "libDoodleJump.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DoodleJump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
